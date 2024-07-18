3b0682544f71cf4b4b7e13f9fa1bca515b84b164
Srija0510 <srijapaul316@gmail.com>
2024-07-18 21:11:34 +0530

    7th commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("login");
+var y= document.getElementById("signup");
+var z = document.getElementById("btn");
+
+
+function signup () {
+    x.style.left = "-400px";
+    y.style.left = "30px";
+    z.style.left = "110px";
+}
+function login () {
+    x.style.left = "35px";
+    y.style.left = "400px";
+    z.style.left = "0px";
+}
+
